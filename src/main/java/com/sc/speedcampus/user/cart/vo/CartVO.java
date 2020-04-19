a88db200fb0a651b79303d8414c2018bcdddec00
yoowonsang <yoowon1860@naver.com>
2020-04-19 13:55:43 +0900

    강좌상세보기에서 나의 장바구니에 추가하기 구현

---
+++ src/main/java/com/sc/speedcampus/user/cart/vo/CartVO.java
@@ -0,0 +1,33 @@
+package com.sc.speedcampus.user.cart.vo;
+
+public class CartVO {
+	private int num;
+	private String email;
+	private String vname;
+	private int price;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getvname() {
+		return vname;
+	}
+	public void setvname(String vname) {
+		this.vname = vname;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+}
