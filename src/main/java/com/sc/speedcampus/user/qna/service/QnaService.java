1c3a7d28f77beeddf953c7f547476c18c37b17e5
dlwltjd7778 <dlwltjd7778@gmail.com>
2020-04-20 00:03:30 +0900

    오타수정

--- src/main/java/com/sc/speedcampus/user/qna/service/QnaService.java
+++ src/main/java/com/sc/speedcampus/user/qna/service/QnaService.java
@@ -8,11 +8,7 @@ public interface QnaService {
 
 	public void insertQ(QnaVO vo);
 	public List<QnaVO> getQList(String writer);
-<<<<<<< HEAD
-	
-=======
 	void updateQ(QnaVO vo);
 	void deleteQ(QnaVO vo);
 	public QnaVO getQ(int qnum);
->>>>>>> 9e1269140f1425ca9a4d9774fba853886037645c
 }
